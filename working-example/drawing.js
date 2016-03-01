e6b6ba72a6271050f1e769ffd84ba8c6ada6bfe2
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-03-01 16:23:17 +0000

    realign canvas in example

--- working-example/drawing.js
+++ working-example/drawing.js
@@ -2,8 +2,8 @@ function drawOnCanvas(circle, randomColor){
   // drawing code here
   //    circle(x-coordinate,y-coordinate);
     var bgColor;
-    for (var i = 0; i < 7; i += 1){
-      for (var j = 0; j < 5; j += 1){
+    for (var i = 1; i < 7; i += 1){
+      for (var j = 1; j < 5; j += 1){
       bgColor = randomColor();
       circle(60 * i,60 * j,bgColor);
       }
